27681f9bc06a47ca059f4835fd8700fa956e4378
iopapamanoglou <ioannis@atopile.io>
2024-09-11 14:58:53 +0200

    Fix mounting hole

--- src/faebryk/library/Mounting_Hole.py
+++ src/faebryk/library/Mounting_Hole.py
@@ -2,50 +2,46 @@
 # SPDX-License-Identifier: MIT
 
 import logging
-from enum import Enum, StrEnum
+from enum import StrEnum, auto
 
 import faebryk.library._F as F
 from faebryk.core.module import Module
+from faebryk.libs.iso_metric_screw_thread import Iso262_MetricScrewThreadSizes
 from faebryk.libs.library import L
 
 logger = logging.getLogger(__name__)
 
 
 class Mounting_Hole(Module):
-    class MetricDiameter(Enum):
-        M2 = ("2.2mm", "_M2")
-        M3 = ("3.2mm", "_M3")
-        M4 = ("4.3mm", "_M4")
-        M5 = ("5.3mm", "_M5")
-        M6 = ("6.4mm", "_M6")
-        M8 = ("8.4mm", "_M8")
-
     class PadType(StrEnum):
         NoPad = ""
-        Pad = "_Pad"
-        TopBottom = "_TopBottom"
-        TopOnly = "_TopOnly"
-        Via = "_Via"
-
-    diameter: MetricDiameter
-    pad_type: PadType
+        Pad = auto()
+        Pad_TopBottom = auto()
+        Pad_TopOnly = auto()
+        Pad_Via = auto()
 
     attach_to_footprint: F.can_attach_to_footprint_symmetrically
     designator_prefix = L.f_field(F.has_designator_prefix_defined)("H")
 
-    def __preinit__(self):
-        # footprint = L.f_field(F.has_footprint_defined)(
-        #    F.KicadFootprint("MountingHole:MountingHole_3.2mm_M3_Pad", pin_names=[])
-        # )
+    def __init__(self, diameter: Iso262_MetricScrewThreadSizes, pad_type: PadType):
+        super().__init__()
+        self._diameter = diameter
+        self._pad_type = pad_type
 
-        # TODO: make back to f_field, rt_field because of imports
-        @L.rt_field
-        def footprint(self):
-            assert self.diameter is not None
-            assert self.pad_type is not None
-            return F.has_footprint_defined(
-                F.KicadFootprint(
-                    f"MountingHole:MountingHole_{self.diameter.value[0]}{self.diameter.value[1] if self.pad_type.value != self.PadType.NoPad else ''}{self.pad_type.value}",  # noqa E501
-                    pin_names=[],
-                )
+    @L.rt_field
+    def footprint(self):
+        size_mm = f"{self._diameter.value:.1f}mm"
+        size_name = self._diameter.name.replace("_", ".")
+        padtype = self._pad_type
+
+        if size_name:
+            size_name = f"_{size_name}"
+        if padtype:
+            padtype = f"_{padtype}"
+
+        return F.has_footprint_defined(
+            F.KicadFootprint(
+                f"MountingHole:MountingHole_{size_mm}{size_name}{padtype}",
+                pin_names=[],
             )
+        )
