6b7ae4fb7472f31f61f7677f4b2165063b952a0f
iopapamanoglou <ioannis@atopile.io>
2024-09-12 19:05:32 +0200

    Reset minimal led

--- examples/minimal_led.py
+++ examples/minimal_led.py
@@ -21,26 +21,14 @@ logger = logging.getLogger(__name__)
 class App(Module):
     led: F.PoweredLED
     battery: F.Battery
-    mcu: F.RP2040_ReferenceDesign
 
     def __preinit__(self) -> None:
-        # self.led.power.connect(self.battery.power)
+        self.led.power.connect(self.battery.power)
 
         # Parametrize
         self.led.led.color.merge(F.LED.Color.YELLOW)
         self.led.led.brightness.merge(
             TypicalLuminousIntensity.APPLICATION_LED_INDICATOR_INSIDE.value.value
-        )
-
-        F.ElectricLogic.connect_all_node_references(
-            [self.led, self.battery, self.mcu], gnd_only=True
-        )
-        # self.mcu.usb.usb_if.buspower.connect(self.battery.power)
-
-        self.mcu.rp2040.i2c[0].sda.signal.connect(self.led.power.hv)
-        self.mcu.rp2040.i2c[0].sda.reference.connect_shallow(self.led.power)
-        self.mcu.rp2040.pinmux.enable(
-            self.mcu.rp2040.i2c[0].sda, pins=list(range(10, 20))
         )
 
 
