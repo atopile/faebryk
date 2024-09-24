3356d9d68bc72eaaeaf66590b53a7004159ab399
ruben-iteng <94007802+ruben-iteng@users.noreply.github.com>
2024-09-24 20:43:38 +0200

    Fix: USB and fused ref and connections

--- src/faebryk/library/CH344Q_ReferenceDesign.py
+++ src/faebryk/library/CH344Q_ReferenceDesign.py
@@ -84,10 +84,7 @@ class CH344Q_ReferenceDesign(Module):
             F.Range.from_center(8 * P.pF, 10 * P.pF)
         )  # TODO: should be property of crystal when picked
 
-        # TODO: hack, fuse current not set
-        # with self.vbus_fused.max_current.merge(F.Range.lower_bound(500 * P.mA))
-        fuse = next(iter(self.vbus_fused.get_children(direct_only=False, types=F.Fuse)))
-        fuse.trip_current.merge(F.Range.upper_bound(500 * P.mA))
+        self.vbus_fused.max_current.merge(F.Range.lower_bound(500 * P.mA))
 
         self.ldo.output_current.merge(F.Range.lower_bound(500 * P.mA))
         self.ldo.output_voltage.merge(F.Range.from_center_rel(3.3 * P.V, 0.05))
