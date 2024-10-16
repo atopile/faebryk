add6c83bd9b94f9d7971a15da50888af15b3f7ee
Sam Mellor <sam@atopile.io>
2024-10-16 14:45:53 -0700

    add file license headers

--- src/faebryk/libs/picker/util.py
+++ src/faebryk/libs/picker/util.py
@@ -1,3 +1,6 @@
+# This file is part of the faebryk project
+# SPDX-License-Identifier: MIT
+
 import faebryk.library._F as F
 from faebryk.core.core import Parameter
 from faebryk.library.Set import Set
