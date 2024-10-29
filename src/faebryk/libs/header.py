b73a347d2c1fa8293566708101ee2bb7a1498b1b
iopapamanoglou <ioannis@atopile.io>
2024-10-29 14:44:11 +0100

    Build: Switch from pybind to nanobind
    nanobind is faster in compiling and executing because it's lighter
    Has better support for pyi stub generation.

---
+++ src/faebryk/libs/header.py
@@ -0,0 +1,9 @@
+# This file is part of the faebryk project
+# SPDX-License-Identifier: MIT
+
+
+def get_header():
+    return (
+        "# This file is part of the faebryk project\n"
+        "# SPDX-License-Identifier: MIT\n"
+    )
