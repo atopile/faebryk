2cb5f44910f85804e9a4661a17a727ada9b877c6
iopapamanoglou <ioannis@atopile.io>
2024-11-03 18:13:36 +0100

    Make more robust; Add lcsc crawl

--- src/faebryk/exporters/documentation/datasheets.py
+++ src/faebryk/exporters/documentation/datasheets.py
@@ -29,45 +29,60 @@ def export_datasheets(
         if not m.has_trait(F.has_datasheet):
             continue
         url = m.get_trait(F.has_datasheet).get_datasheet()
-        if url:
-            filename = type(m).__name__ + ".pdf"
-            file_path = path / filename
-            if file_path.exists() and not overwrite:
-                logger.info(
-                    f"Datasheet for {m.get_full_name(types=False)} already exists, skipping download"  # noqa: E501
-                )
-                continue
-            success = _download_datasheet(url, file_path)
-            logger.info(
-                f"Downloaded datasheet for {m.get_full_name(types=False)}: {'\033[92mOK\033[0m' if success else '\033[91mFAILED\033[0m'}"  # noqa: E501
+        if not url:
+            logger.warning(f"Missing datasheet URL for {m}")
+            continue
+        filename = type(m).__name__ + ".pdf"
+        file_path = path / filename
+        if file_path.exists() and not overwrite:
+            logger.debug(
+                f"Datasheet for {m} already exists, skipping download"  # noqa: E501
             )
+            continue
+        try:
+            _download_datasheet(url, file_path)
+        except DatasheetDownloadException as e:
+            logger.error(f"Failed to download datasheet for {m}: {e}")
+
+        logger.debug(f"Downloaded datasheet for {m}")
 
 
-def _download_datasheet(url: str, path: Path) -> bool:
+class DatasheetDownloadException(Exception):
+    pass
+
+
+def _download_datasheet(url: str, path: Path):
     """
     Download the datasheet of the given module and save it to the given path.
     """
     if not url.endswith(".pdf"):
-        logger.warning(f"Datasheet URL {url} is probably not a PDF")
-        return False
+        raise DatasheetDownloadException(f"Datasheet URL {url} is probably not a PDF")
     if not url.startswith(("http://", "https://")):
-        logger.warning(f"Datasheet URL {url} is probably not a valid URL")
-        return False
+        raise DatasheetDownloadException(
+            f"Datasheet URL {url} is probably not a valid URL"
+        )
 
     try:
+        # TODO probably need something fancier
         user_agent_headers = {
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"  # noqa: E501
         }
         response = requests.get(url, headers=user_agent_headers)
         response.raise_for_status()
     except requests.RequestException as e:
-        logger.error(f"Failed to download datasheet from {url}: {e}")
-        return False
+        raise DatasheetDownloadException(
+            f"Failed to download datasheet from {url}: {e}"
+        ) from e
+
+    # check if content is pdf
+    if not response.content.startswith(b"%PDF"):
+        raise DatasheetDownloadException(
+            f"Downloaded content is not a PDF: {response.content[:100]}"
+        )
 
     try:
         path.write_bytes(response.content)
     except Exception as e:
-        logger.error(f"Failed to save datasheet to {path}: {e}")
-        return False
-
-    return True
+        raise DatasheetDownloadException(
+            f"Failed to save datasheet to {path}: {e}"
+        ) from e
