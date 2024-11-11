cde150c2892a399b87dc7b73d8352136252b6323
napowderly <napowderly@gmail.com>
2024-11-11 13:17:54 -0800

    add new line

--- examples/minimal_led.py
+++ examples/minimal_led.py
@@ -32,6 +32,7 @@ class App(Module):
             TypicalLuminousIntensity.APPLICATION_LED_INDICATOR_INSIDE.value.value
         )
 
+
 # Boilerplate -----------------------------------------------------------------
 
 
